56d717a29ca34ae85b2dd4f3896e547c4ac6f278
pgomez04 <pgomez04@alumnos.uaq.mx>
2023-01-25 18:40:37 -0600

    comentario

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ static int I2C_wait_till_done(void);
 char I2C3_Write_Multiple(int slave_address, char slave_memory_address, int bytes_count, uint8_t* data);
 char I2C3_Wr(int slaveAddr, char memAddr, uint8_t data);
 
-// MAIN
+// FUNCION PRINCIPAL 
 int main(void)
 {
 	 I2C3_Init();
